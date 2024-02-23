5b86eaeff1eb9ed56a4e9283f3feac5dd0b03c79
mdfahimhosen79 <mdfahimhosen79@gmail.com>
2024-02-23 11:52:25 +0600

    dsa

--- Basic Data Structure/Module_17/code.cpp
+++ Basic Data Structure/Module_17/code.cpp
@@ -1,51 +1,72 @@
 #include <bits/stdc++.h>
 using namespace std;
-class Node
-{   
+class Node{
     public:
-    int val;
-    Node* left;
-    Node* right;
-    Node(int val)
-    {
+     int val;
+     Node * left;
+     Node * right;
+     Node(int val)
+     {
         this->val = val;
         this->left = NULL;
         this->right = NULL;
-    }
-}; 
-void postorder(Node* root)
+     }
+};
+void levelorder(Node* root)
 {
-    // base case
-    if(root == NULL)
-    {
-        return;
+    if(root == NULL) return;
+    queue<Node*> q;
+    q.push(root);
+    while(!q.empty())
+    {   
+        // ber kore ana
+        Node * f = q.front();
+        q.pop();
+        // jabotio ja kaj
+        cout<<f->val<<" ";
+        // children gula ke line(queue) e dao
+        if(f->left != NULL) q.push(f->left); 
+        if(f->right != NULL) q.push(f->right); 
     }
-    postorder(root->left);
-    postorder(root->right);
-    cout<<root->val<<" ";    
+}
+Node* input_tree()
+{
+    int val;
+    cin>>val;
+    Node * root;
+    if(val == -1) root = NULL; 
+    else root = new Node(val);
+
+    queue <Node*> a;
+    if(root!=NULL) a.push(root);
+
+    while(!a.empty())
+    {
+        // 1st step
+        Node* p = a.front();
+        a.pop();
+
+        // 2nd step
+        int l,r;
+        cin>>l>>r;
+        Node * myleft,*myright;
+        if(l != -1) myleft = new Node(l);
+        else myleft = NULL;
+        if(r==-1) myright = NULL;
+        else myright = new Node(r);
+
+        p->left = myleft;
+        p->right = myright;
+        
+        // 3rd step;
+        if(p->left != NULL) a.push(p->left);
+        if(p->right != NULL) a.push(p->right);
+    }
+    return root;
 }
 int main()
 {
-    Node* root = new Node(10);
-    Node* a = new Node(20);
-    Node* b = new Node(30);
-    Node* c = new Node(40);
-    Node* d = new Node(50);
-    Node* e = new Node(60);
-    Node* f = new Node(70);
-    Node* g = new Node(80);
-    Node* h = new Node(90);
-    Node* i = new Node(100);
-    // connection
-    root->left = a;
-    root->right = b;
-    a->left = c;
-    a->right = h;
-    c->right = e;
-    h->right = i;
-    b->right = d;
-    d->left = f;
-    d->right = g;
-    postorder(root);
+    Node * root = input_tree();
+    levelorder(root);
     return 0;
-}
+} 
